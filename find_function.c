48adb5c36c9ae4b53e6f64ec74c9a5a99198952e
seObando19 <sebastiano-10@hotmail.com>
2020-07-19 16:15:59 +0000

    Added file for find the functions we need

---
+++ find_function.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+int (*find_function(char letter))(va_list list)
+{
+	_formats ops[] ={
+		{'c', print_char},
+		/* {'s', print_string},
+		{'i', print_integer} */
+	};
+	int i;
+
+	for (i = 0; i < 1; i++)
+	{
+		if (ops[i].format == letter)
+		{
+			return (ops[i].function);
+		}
+	}
+	return (0);
+}
