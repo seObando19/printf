2fedb3c2fa0ab0e81517246b90d53682fcbca271
seObando19 <sebastiano-10@hotmail.com>
2020-07-19 16:15:12 +0000

    Updated header file with new structure and header of functions

--- holberton.h
+++ holberton.h
@@ -3,6 +3,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <unistd.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
+int (*find_function(char letter))(va_list list);
+int valid_format(char c);
+int print_char(va_list list);
+/* int print_string(va_list list);
+int print_integer(va_list list); */
+typedef struct typeformat
+{
+	char format;
+	int (*function)(va_list);
+}_formats;
 #endif /* _HOLBERTON_H_ */
