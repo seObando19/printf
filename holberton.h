959905a215b56689c86f5a7a8740bb8536926c54
seObando19 <sebastiano-10@hotmail.com>
2020-07-19 00:03:00 +0000

    add header file

---
+++ holberton.h
@@ -0,0 +1,8 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif /* _HOLBERTON_H_ */
