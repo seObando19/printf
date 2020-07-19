e070ba85eea747725586b2632a056c78e78660dc
seObando19 <sebastiano-10@hotmail.com>
2020-07-19 16:14:36 +0000

    Add file for print strings

--- print.c
+++ print.c
@@ -4,3 +4,18 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+int print_char(va_list params)
+{
+	char m;
+
+	m = (char) va_arg(params, int);
+	return (write(1, &m, sizeof(char)));
+}
+int print_string(va_list params)
+{
+	int i;
+	char *ptr;
+
+	for (i = 0;)
+}
