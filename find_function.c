c2ca1ad75790af0cb2ead74564a80651f15e130d
seObando19 <sebastiano-10@hotmail.com>
2020-07-20 23:28:09 +0000

    Updated file for search of  function print_rev

--- find_function.c
+++ find_function.c
@@ -12,12 +12,12 @@ int (*find_function(const char letter))(va_list list)
 		{'s', print_string},
 		{'i', print_integer},
 		{'d', print_integer},
-		/* {'r', print_rev}, */
+		{'r', print_rev},
 		{'\0', NULL}
 	};
 	int i;
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 6; i++)
 	{
 		if (ops[i].format == letter)
 		{
