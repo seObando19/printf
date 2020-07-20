61c762406e5aec93251f09bffc7766f5ecbf677c
seObando19 <sebastiano-10@hotmail.com>
2020-07-20 23:29:45 +0000

    Added new file with the function print_rev

---
+++ print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+   *print_rev - print reverse of string
+   *@params:arguments to print
+   *Return:string reverse
+ */
+int print_rev(va_list params)
+{
+	int i = 0, a, lenght = 0;
+	char *ptr, *newStrg;
+
+	ptr = va_arg(params, char*);
+	while (ptr[i] != '\0')
+		i++;
+	lenght = i;
+	newStrg = malloc(sizeof(char) * lenght + 1);
+	if (ptr != NULL && ptr != 0 && newStrg != NULL)
+	{
+		for (a = 0; a <= i; a++)
+		{
+			newStrg[a] = ptr[lenght];
+			lenght--;
+		}
+		return (write(1, newStrg, a));
+	}
+	return (write(1, "(null)", 6));
+}
