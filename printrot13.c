16b9e44c5ed693038e1edb70f050f498db5e9fcc
juanesart <juanesartq@gmail.com>
2020-07-21 00:23:37 +0000

    Create print_rot13 function

---
+++ printrot13.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * print_rot13 - prints a string in rot13
+ * @params:arguments to print
+ * Return: number of chars printed
+ */
+
+int print_rot13(va_list params)
+{
+        char * ptr;
+        unsigned i, j;
+        int cont;
+        char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+        ptr = va_arg(params, char *);
+        if (ptr != NULL)
+        {
+                for (i = 0; ptr[i]; i++)
+                {
+                        for (j = 0; input[j]; j++)
+                        {
+                                if (input[j] == ptr[i])
+                                {
+                                        _putchar(output[j]);
+                                        cont++;
+                                }
+                        }
+                        if (!input[j])
+                        {
+                                _putchar(ptr[i]);
+                                cont++;
+                        }
+                }
+                return (cont);
+        }
+        return (write(1, "(null)", 6));
+}
