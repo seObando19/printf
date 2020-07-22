cafd7163ec14bb44ab219e60c0b0ccf8b1dd5564
seObando19 <sebastiano-10@hotmail.com>
2020-07-22 23:11:14 +0000

    Updated in the identation of the code

--- printbin.c
+++ printbin.c
@@ -11,35 +11,35 @@ int print_bin(va_list list)
 	unsigned int integer;
 	int out[64];
 	int i = 0, j = 0;
-        int cont = 0;
-        integer = va_arg(list, int);
+	int cont = 0;
 
-        if (integer == 0)
-        {
-                _putchar('0' + 0);
-                cont++;
-                return (cont);
-        }
+	integer = va_arg(list, int);
+	if (integer == 0)
+	{
+		_putchar('0' + 0);
+		cont++;
+		return (cont);
+	}
 
-        while (integer >= 1)
-        {
-                if (integer % 2 == 0)
-                {
-                        out[i] = 0;
-                        integer = integer / 2;
-                        i++;
-                }
-                else
-                {
-                        out[i] = 1;
-                        integer = integer / 2;
-                        i++;
-                }
-        }
-        for (j = i - 1; j >= 0; j--)
-        {
-                _putchar('0' + out[j]);
-                cont++;
-        }
-        return (cont);
+	while (integer >= 1)
+	{
+		if (integer % 2 == 0)
+		{
+			out[i] = 0;
+			integer = integer / 2;
+			i++;
+		}
+		else
+		{
+			out[i] = 1;
+			integer = integer / 2;
+			i++;
+		}
+		}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar('0' + out[j]);
+		cont++;
+	}
+	return (cont);
 }
