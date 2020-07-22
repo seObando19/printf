39bbb422da8b3fd782359d91124d0569307a7e71
juanesart <juanesartq@gmail.com>
2020-07-22 01:35:00 +0000

    Create print_bin function

---
+++ printbin.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * print_bin - prints a string in bin
+ * @list:arguments to print
+ * Return: number of chars printed
+ */
+
+int print_bin(va_list list)
+{
+	unsigned int integer;
+	int out[64];
+	int i = 0, j = 0;
+        int cont = 0;
+        integer = va_arg(list, int);
+
+        while (integer >= 1)
+        {
+                if (integer % 2 == 0)
+                {
+                        out[i] = 0;
+                        integer = integer / 2;
+                        i++;
+                }
+                else
+                {
+                        out[i] = 1;
+                        integer = integer / 2;
+                        i++;
+                }
+        }
+        for (j = i - 1; j >= 0; j--)
+        {
+                _putchar('0' + out[j]);
+                cont++;
+        }
+        return (cont);
+}
